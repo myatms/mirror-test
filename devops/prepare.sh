5e37959b3a050d7056988da74abafd3e1157be68
EC2 Default User <ec2-user@ip-10-0-31-149.ap-southeast-1.compute.internal>
2023-08-16 15:27:58 +0000

    update

--- devops/prepare.sh
+++ devops/prepare.sh
@@ -4,7 +4,7 @@ touch /tmp/deployment-started
 
 aws s3 cp s3://mms-codedeploy/env.txt /tmp/env.txt
 aws s3 cp s3://mms-codedeploy/nginx.conf /tmp/nginx.conf
-sudo mkdir -p /var/www/html/
+sudo mkdir -p /var/www/html/bagisto
 sudo chown -R ec2-user:ec2-user /var/www/html/
 sudo chmod -R 775 /var/www/html/
 
